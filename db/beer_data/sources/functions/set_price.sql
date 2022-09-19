ad20f5260269217254e2ec9cdcc8f07fc1c8fd54
jaw <Jan.Winkels@opitz-consulting.com>
2022-09-19 17:59:34 +0200

    Version 1.1

--- db/beer_data/sources/functions/set_price.sql
+++ db/beer_data/sources/functions/set_price.sql
@@ -2,17 +2,22 @@ create or replace function set_price (p_price number) return boolean
 is
   l_rows number;
 begin
+  
   select count(*)
   into l_rows
   from settings;
+  
   if l_rows = 1 then
     update settings set price = p_price;
   else
     insert into settings(price) values(p_price);
   end if;
+  
   return true;
+  
   exception
     when others then
       logger.log(p_text=>'Set price failed', p_extra=>sqlerrm);
       return false;
 end set_price;
+/
